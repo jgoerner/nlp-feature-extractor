f41daceb5caacb8df4b924b8e16876f873f8ee7d
Joshua Görner <joshua.goerner@gmail.com>
2017-11-05 01:59:48 +0100

    Update utils.py

--- extractors/utils.py
+++ extractors/utils.py
@@ -5,5 +5,5 @@ def fu_to_df(feature_union, X):
     # number of features
     n_feat = len(feature_union.transformer_list)
     # names of transformer
-    name_trans = [n for n, _ in fu.transformer_list]
-    return pd.DataFrame(fu.transform(X).reshape(n_feat, -1).T, columns=name_trans)
+    name_trans = [n for n, _ in feature_union.transformer_list]
+    return pd.DataFrame(feature_union.transform(X).reshape(n_feat, -1).T, columns=name_trans)
