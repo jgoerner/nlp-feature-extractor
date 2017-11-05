43752605d3aedf51ff4585a0a9085db27c02fb43
Joshua Görner <joshua.goerner@gmail.com>
2017-11-05 09:50:39 +0100

    Update utils.py
    Will return well converted dtype

--- extractors/utils.py
+++ extractors/utils.py
@@ -1,9 +1,12 @@
-from sklearn.pipeline import FeatureUnion
-
 def fu_to_df(feature_union, X):
-    """Convert result of a feature union to pandas df"""
     # number of features
     n_feat = len(feature_union.transformer_list)
     # names of transformer
     name_trans = [n for n, _ in feature_union.transformer_list]
-    return pd.DataFrame(feature_union.transform(X).reshape(n_feat, -1).T, columns=name_trans)
+    # initially all "object"
+    df_raw = pd.DataFrame(feature_union.transform(X).reshape(n_feat, -1).T, columns=name_trans)
+    # try to convert all numeric columns to numeric dtypes
+    df_cleaned_dtypes = df_raw.copy()
+    for col in df_cleaned_dtypes.columns:
+        df_cleaned_dtypes[col] = pd.to_numeric(df_cleaned_dtypes[col], errors="ignore")
+    return df_cleaned_dtypes
