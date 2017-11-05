326a7da559fd70c6d267d948079229a418c0403e
Joshua Görner <joshua.goerner@gmail.com>
2017-11-05 09:51:46 +0100

    Update utils.py

--- extractors/utils.py
+++ extractors/utils.py
@@ -1,4 +1,5 @@
 def fu_to_df(feature_union, X):
+    """Helper to convert the result of a feature union to dataframe"""
     # number of features
     n_feat = len(feature_union.transformer_list)
     # names of transformer
@@ -10,3 +11,10 @@ def fu_to_df(feature_union, X):
     for col in df_cleaned_dtypes.columns:
         df_cleaned_dtypes[col] = pd.to_numeric(df_cleaned_dtypes[col], errors="ignore")
     return df_cleaned_dtypes
+
+def one_hot_df(df, ignore=[], verbose=True):
+    """Helper to convert str columns in a one hot way"""
+    columns = df.drop(ignore, axis=1).select_dtypes(["object"]).columns.values
+    if verbose:
+        print("Following columns one hot encoded: {}".format(columns))
+    return pd.get_dummies(df, columns=columns)
